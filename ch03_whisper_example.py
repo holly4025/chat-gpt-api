6245cd277b00487743b916250ac72a8ca574f57f
Bae Hye In <111195325+holly4025@users.noreply.github.com>
2024-05-01 13:19:44 +0900

    Update ch03_whisper_example.py

--- ch03_whisper_example.py
+++ ch03_whisper_example.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "sk-proj-9J4Dw9QrzfO9Tale0FyNT3BlbkFJQjx1xZLzK9elXFO0HqlO"
+openai.api_key = "openai API KEY"
 
 audio_file = open('output.mp3', 'rb')
 transcript = openai.Audio.transcribe('whisper-1', audio_file)
